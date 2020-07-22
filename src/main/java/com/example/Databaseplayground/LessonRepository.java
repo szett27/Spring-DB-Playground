24f2554304e281933efed4bb67fa0dbd56c671a5
Scott <scott.zetterstrom@gmail.com>
2020-07-22 14:33:37 -0600

    Added CRUD Get with id

--- src/main/java/com/example/Databaseplayground/LessonRepository.java
+++ src/main/java/com/example/Databaseplayground/LessonRepository.java
@@ -1,8 +1,10 @@
 package com.example.Databaseplayground;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LessonRepository extends CrudRepository<Lesson, Long> {
 
-
+    @Query(value = "SELECT * FROM lessons WHERE id = :id", nativeQuery = true)
+    public Lesson byId(Long id);
 }
--- src/main/java/com/example/Databaseplayground/LessonsController.java
+++ src/main/java/com/example/Databaseplayground/LessonsController.java
@@ -2,6 +2,8 @@ package com.example.Databaseplayground;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
+
 @RestController
 @RequestMapping("/lessons")
 public class LessonsController {
@@ -21,5 +23,11 @@ public class LessonsController {
     public Lesson create(@RequestBody Lesson lesson) {
         return this.repository.save(lesson);
     }
+
+    @GetMapping("/{id}")
+    public Lesson getlesson(@PathVariable("id") Long id) {
+        return this.repository.byId(id);
+    }
+
 
 }
