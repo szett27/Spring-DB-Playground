30164ff316075b43639a7d4ca01cf8d58c4c55de
Scott <scott.zetterstrom@gmail.com>
2020-07-22 13:58:01 -0600

    Initial Commit-db playground works

---
+++ src/main/java/com/example/Databaseplayground/DatabasePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.Databaseplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasePlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasePlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Databaseplayground/DatabasePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Databaseplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasePlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
